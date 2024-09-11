967375bbc228bb99b716ff77d46e7731d93cbf15
NicolyFinkler <160048407+NicolyFinkler@users.noreply.github.com>
2024-09-11 14:46:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'HISTORIA',
+    'Quem descobriu o Brasil?',
+    'Brasil foi descoberto por Pedro Alvares Cabral'
+)
+
+criaCartao(
+    'GEOGRAFIA',
+    'Qual maior deserto do mundo?',
+    'Deserto do Saara'
+)
+
+criaCartao(
+    'ESPORTE',
+    'Quantos jogadores tem em um time de futebol',
+    'Composto por 11 jogadores'
+)
+
+criaCartao(
+    'ARTE',
+    'Quem pintou a Mona Lisa?',
+    'Foi pintada por Leonardo da Vinci'
+)
