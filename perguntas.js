84112ba27abb4b1fb0345c14cf77fa3b34eee910
NicolyFinkler <160048407+NicolyFinkler@users.noreply.github.com>
2024-09-18 14:32:22 -0300

    qua

--- perguntas.js
+++ perguntas.js
@@ -21,3 +21,37 @@ criaCartao(
     'Quem pintou a Mona Lisa?',
     'Foi pintada por Leonardo da Vinci'
 )
+
+criaCartao(
+    'HISTORIA',
+    'Em que ano ocorreu a Revolução Francesa?',
+    '1789'
+)
+
+criaCartao(
+    'MATEMATICA',
+    'Qual o resultado de 9 vezes 8?',
+    '72'
+)
+
+criaCartao(
+    'TECNOLOGIA',
+    'uem é conhecido como pai da computação?',
+    'Alan Turing'
+)
+criaCartao(
+    'LITERATURA',
+    'Quem escreveu Dom Quixote?',
+    'Miguel de Cervantes'
+)
+
+criaCartao(
+    'MÚSICA',
+    'Quem é conhecido como rei do Rock?',
+    'Elvis Presley'
+)
+criaCartao(
+    'CINEMA',
+    'Qual filme ganhou o Oscar de Melhor Filme em 1994?',
+    'Forrest Gump'
+)
